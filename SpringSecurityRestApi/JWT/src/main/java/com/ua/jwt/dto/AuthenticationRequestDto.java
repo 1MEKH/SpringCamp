836a8cf377279ae45143ea69e6051fd55a51c13f
Artem <98288627+1MEKH@users.noreply.github.com>
2022-07-24 23:33:48 +0300

    Update AuthenticationRequestDto.java

--- SpringSecurityRestApi/JWT/src/main/java/com/ua/jwt/dto/AuthenticationRequestDto.java
+++ SpringSecurityRestApi/JWT/src/main/java/com/ua/jwt/dto/AuthenticationRequestDto.java
@@ -6,4 +6,11 @@ import lombok.Data;
 public class AuthenticationRequestDto {
     private String username;
     private String password;
+    
+    public static User toUser(AuthenticationRequestDto userDto){
+        User user = new User();
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        return user;
+    }
 }
